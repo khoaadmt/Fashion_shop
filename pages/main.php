4e591bc9393bc169ca6982886c6e4034809d6d3b
skt17mm <94218988+skt17mm@users.noreply.github.com>
2023-08-19 10:29:59 +0700

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -37,27 +37,39 @@
                         }else{ ?>
 
 
-<script>
-                let slideIndex = 0;
-                showSlides();
+<div class="banner">
+        <div class="slideshow-container">
+            <div class="mySlides fade">
+                <img src="images/banner.jpg" alt="Banner 1" style="width:100%">
+            </div>
+            <div class="mySlides fade">
+                <img src="images/1a.jpg" alt="Banner 2" style="width:100%">
+            </div>
+            <div class="mySlides fade">
+                <img src="images/4a.jpg" alt="Banner 3" style="width:100%">
+            </div>
+            <!-- Add more slides if needed -->
+        </div>
+    </div>
 
-                function showSlides() {
-                let i;
-                let slides = document.getElementsByClassName("mySlides");
-                let dots = document.getElementsByClassName("dot");
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = "none";  
-                }
-                slideIndex++;
-                if (slideIndex > slides.length) {slideIndex = 1}    
-                for (i = 0; i < dots.length; i++) {
-                    dots[i].className = dots[i].className.replace(" active", "");
-                }
-                slides[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " active";
-                setTimeout(showSlides, 3000); // Change image every 2 seconds
-                }
-</script>           
+    <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {
+                slideIndex = 1;
+            }
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 3000); // Change image every 3 seconds
+        }
+    </script>           
                         <?php
                        
                         }
